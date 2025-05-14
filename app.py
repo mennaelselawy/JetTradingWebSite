8b9c931d459ffbbe52c358d5b7d716f592ae276a
menna <mennnnaashraf855@gmail.com>
2025-05-14 09:03:54 +0300

    second commit with the devices and add to cart feature

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import flask 
-from flask import redirect, request,  url_for  
+from flask import redirect, request,  url_for ,jsonify, Response , Flask
 import json
+
 #import os
 app = flask.Flask("app")
 
@@ -12,6 +13,7 @@ def getHtml(pageName):
     # htmlPage.close()
     # return pageContent
 
+                                                        # USER
 class User:
     def __init__(self):
         self.username = flask.request.form.get("username")    #flask-> module , request-> object , form -> property, get-> method 
@@ -59,14 +61,51 @@ class User:
             users =[]
         for user in users:
             if (user["email"] == self.email) and (user["password"] == self.password):
-                return redirect(url_for('loggedInHomePage'))
+                return redirect(url_for('loggedInHomePage', email=self.email))
         
         # login failed — show error
         failedLogging = getHtml("login")
         return failedLogging.replace("$$ERROR$$", "<p style='color:red;'>Login failed: Wrong email or password</p>")
     
-    def updateUserInfo():
+    def updateUserInfo(self):
+        # data = flask.request.form.to_dict()            #.get_json()parse JSON data from the body of an HTTP request (like an API) converts it into a Python dictionary
+        try:
+            with open("data/users.json", "r") as f:
+                users =json.load(f)
+        except(FileNotFoundError, json.JSONDecodeError):
+            failedPage = getHtml("profile")
+            return failedPage.replace("$$ERROR$$", "<p style='color:red;'>Update Failed </p>")
         
+        for i in range(len(users)):
+            # user = users[i]
+            if users[i]["email"] == self.email:
+                users[i] = self.makeDictionary()
+                break
+
+        with open("data/users.json" , "w") as f:
+            json.dump(users, f, indent=4)
+        successPage = getHtml("profile")
+        return successPage.replace("$$ERROR$$", "<p style='color:green;'>User updated successfully</p>")
+    
+    def deleteUser(self):
+        try:
+            with open("data/users.json", "r") as f:
+                users = json.load(f)
+        except(FileNotFoundError , json.JSONDecodeError):
+            users=[]
+        for user in users:
+            if user["email"] == self.email:
+                users.remove(user)
+                break
+        # updated_users=[]
+        # for user in users:
+        #     if user["email"] != self.email:
+        #         updated_users.append(user)
+        with open("data/users.json", "w") as f:
+            json.dump(users, f, indent=4)
+        return redirect(url_for("homePage"))
+
+
 
 @app.route("/")
 def homePage():
@@ -96,6 +135,201 @@ def logIn():
 def loggedInHomePage():
     return getHtml("loggedInHome")
 
+
 @app.route("/profile", methods=["GET"])
 def profilePage():
-    return getHtml("profile")
+    html = getHtml("profile")
+    return html.replace("$$ERROR$$", "")
+#HTML Forms do not suppory put or patch or delete for update or delete 
+@app.route("/profile" , methods=["POST"])
+def updateUser():
+    user = User()
+    return user.updateUserInfo()
+
+@app.route("/delete", methods=["POST"])
+def deleteUserAccount():
+    user = User()
+    return user.deleteUser()
+
+
+
+                                                 #DEVICES
+class Devices:
+    def __init__(self, name, description, price, image , category):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.image = image
+        self.category = category
+
+# make each device dictionary to convert to JSON format if i want to deal with js so i will need it json
+    def make_dictionary(self):
+        dict={
+            "name": self.name,
+            "description": self.description,
+            "price": self.price,
+            "image": self.image,
+            "category": self.category,
+        }
+        return dict
+
+# here i parse the text files to python objects of the class to can deal with it as search/filter
+#return list  to deal with html i use lists
+    @staticmethod                               #only reads the lines from a file and convert them into Devices objects using constructor
+    def parse_products_file():
+        products =[]
+        try:
+            with open("data/products.txt", "r") as f:
+                for line in f:
+                    property = line.strip().split("|")
+                    if len(property) == 5:
+                        product = Devices(property[0], property[1], property[2], property[3], property[4])     #it creates objects from the class devices
+                        products.append(product)
+        except(FileNotFoundError):
+            products=[]
+        return products
+    
+    @staticmethod
+    def display_devices_html(devices_list, show_cart_buttons=False):
+        html=""
+        for device in devices_list: 
+            # f """" = formatted string literals embed expressions directly into string literals using {}
+            # i used the data- attribute so in javascript will get it by dataset.
+            html += f""" 
+                <div class="device-card">  
+                    <img src="static/images/{device.image}" alt="{device.name}" class="device-img">
+                    <h3 class="device-name">{device.name}</h3>
+                    <p class="device-description">{device.description}</p>
+                    <p class="device-price">EGP {device.price}</p> """
+            if show_cart_buttons:    
+                html += f""" <button class="add-to-cart-button"
+                        data-name = "{device.name}"
+                        data-description = "{device.description}"
+                        data-price = "{device.price}"
+                        data-image = "{device.image}">
+                        Add to Cart
+                    </button>"""
+            html += "</div>"
+        return html
+
+        
+@app.route("/devices", methods=["GET"])
+def devicesPage():
+    devices = Devices.parse_products_file()
+    category = request.args.get('category')
+    query = request.args.get('query')
+    heading =""
+    filtered = []
+    if query:
+        heading = f"Results of {query}"
+        query = query.lower()
+        for device in devices:
+            if query in device.name.lower() or query in device.description.lower():
+                filtered.append(device)
+    
+    elif category:
+        heading = category.capitalize()
+        category = category.lower()
+        for device in devices:
+            if device.category.lower() == category:
+                filtered.append(device)  
+    else:
+        filtered = devices
+        heading = "All Devices"
+
+    devices_html = Devices.display_devices_html(filtered, show_cart_buttons=False)
+    
+    if not devices_html:
+        devices_html = "<p class='no-results'>No devices found.</p>"
+    
+    html = getHtml("devices")
+    final_html = html.replace("$$CATEGORY$$", heading).replace("$$DEVICES$$", devices_html)
+    return final_html
+  
+    # devices_dicts = [device.make_dictionary() for device in devices]
+    # devices_dicts = []
+    # for device in devices:
+    #     device_dict = device.make_dictionary()
+    #     devices_dicts.append(device_dict)
+    # return jsonify(devices_dicts)       #jsonify to convert the dict's to json format 
+
+
+# @app.route("/devices", methods=["GET"])
+# def category_devices():
+#     devices = Devices.parse_products_file()
+#     category = request.args.get('category').lower()
+#     filtered_category =[]
+#     for device in devices:
+#         if device.category.lower() == category:
+#             filtered_category.append(device)
+#     devices_html = Devices.display_devices_html(filtered_category)
+#     html=getHtml("devices")
+#     final_html = html.replace("$$DEVICES$$", devices_html)
+#     return final_html
+
+@app.route("/devicesLoggedIn", methods=["GET"])
+def devicesLoggedInPage():
+    devices = Devices.parse_products_file()
+    category = request.args.get('category')
+    query = request.args.get('query')
+    heading =""
+    filtered = []
+    if query:
+        heading = f"Results of {query}"
+        query = query.lower()
+        for device in devices:
+            if query in device.name.lower() or query in device.description.lower():
+                filtered.append(device)
+    
+    elif category:
+        heading = category.capitalize()
+        category = category.lower()
+        for device in devices:
+            if device.category.lower() == category:
+                 filtered.append(device)  
+    else:
+        filtered = devices
+        heading = "All Devices"
+
+    devices_html = Devices.display_devices_html(filtered, show_cart_buttons=True)
+    if not devices_html:
+        devices_html = "<p class='no-results'>No devices found.</p>"
+    
+    html = getHtml("devicesLoggedIn")
+    final_html = html.replace("$$CATEGORY$$", heading).replace("$$DEVICES$$", devices_html)
+    return final_html
+
+
+
+
+                                               #CART
+class Cart:
+    def __init__(self):
+        self.items=[]
+    
+    def add_product(self, product):
+        self.items.append(product)
+    def remove_product(self, product_name):
+        for item in self.items:
+            if item.name != product_name:
+                self.items =item
+    def make_dictionary(self):
+        for item in self.item:
+            return item.make_dictionary()
+    def clear_cart(self):
+        self.items = []
+    def get_items(self):
+        return self.items
+    
+@app.route("/products_json")
+def products_json():
+    products_list = Devices.parse_products_file()
+    devices_dicts = []
+    for device in products_list:
+        devices_dicts.append(device.make_dictionary())
+    return jsonify(devices_dicts)
+
+@app.route("/cart")
+def cartPage():
+    html = getHtml("cart")
+    return html
--- static/script.js
+++ static/script.js
@@ -1,5 +1,8 @@
+                                                   // USER //
 const signUpForm = document.getElementById("signupForm");
 const profileForm = document.getElementById("profileForm");
+const loginForm = document.getElementById("loginForm");
+const deleteForm = document.getElementById("deleteForm");
 
 function saveUserDataToLocalStorage(){
     //event.preventDefault(); event   //stop the default browser from submitting the form by html, So do it here manually by javascript
@@ -11,7 +14,6 @@ function saveUserDataToLocalStorage(){
     const address = document.getElementById("address").value.trim();
     const phone = document.getElementById("phone").value.trim();
 
-    
     //to build an object in js as a struct in c++
     const user = {
         username,
@@ -30,18 +32,29 @@ function saveUserDataToLocalStorage(){
         localStorage.setItem("users", JSON.stringify(users));
     }
 }
-
 if(signUpForm){
     signUpForm.addEventListener("submit", saveUserDataToLocalStorage);
 }
 
-
+function storeTheLoggedInUserEmail(){
+    const email = document.getElementById("email").value.trim();
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    const loggedInUser = users.find(u => u.email === email);
+    if(loggedInUser){
+        localStorage.setItem("currentUserEmail", loggedInUser.email);
+        localStorage.setItem("currentUser", JSON.stringify(loggedInUser)); 
+    }
+}
+if(loginForm){
+    loginForm.addEventListener("submit", storeTheLoggedInUserEmail);
+}
 
 
 function showUserDataToEdit(){
-    let users= JSON.parse(localStorage.getItem("users")) || [];  
-    const currentUserEmail = localStorage.getItem("email");
-    const user = users.find(u=>u.email === currentUserEmail);
+    // let users= JSON.parse(localStorage.getItem("users")) || [];  
+    // const currentUserEmail = localStorage.getItem("currentUserEmail");
+    // const user = users.find(u=>u.email === currentUserEmail);
+    const user = JSON.parse(localStorage.getItem("currentUser"));
     if(user){
         document.getElementById("username").value = user.username;
         document.getElementById("email").value = user.email;
@@ -50,7 +63,6 @@ function showUserDataToEdit(){
         document.getElementById("phone").value = user.phone;
     }
 }
-
 function updateUserDataToLocalStorage(){
     const updatedUser ={
         username: document.getElementById("username").value,
@@ -61,11 +73,219 @@ function updateUserDataToLocalStorage(){
     };
 
     let users = JSON.parse(localStorage.getItem("users")) || [];
-    
+    const currentEmail = localStorage.getItem("currentUserEmail");
+    for(let i = 0; i< users.length; i++){
+        if(users[i].email === currentEmail){
+            users[i] = updatedUser;
+            break;
+        }
+    }
+    localStorage.setItem("users" , JSON.stringify(users));
+    localStorage.setItem("currentUser", JSON.stringify(updatedUser)); 
 }
-
-if(profileForm){
+document.addEventListener("DOMContentLoaded", function(){
     showUserDataToEdit();
+    if(profileForm){
     profileForm.addEventListener("submit" , updateUserDataToLocalStorage);
+    }
+});
+
+
+function logout() {
+    localStorage.removeItem("currentUserEmail");
+    localStorage.removeItem("currentUser");
+    window.location.href = "/";
 }
 
+function enableEdit(fieldId) {
+    document.getElementById(fieldId).removeAttribute("readonly");
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+    const user = JSON.parse(localStorage.getItem("currentUser"));
+    if(user){
+        document.getElementById("welcomeUser").textContent = user.username;
+    }
+});
+
+function deleteCurrentUserFromLocalStorage(){
+    const currentEmail = localStorage.getItem("currentUserEmail");
+    if(!currentEmail) return;
+    let users = JSON.parse(localStorage.getItem("users")) || [];
+    let newUsers =[];
+    for(let i=0; i<users.length; i++){
+        if(users[i].email !== currentEmail){
+            newUsers.push(users[i]);
+        }
+    }
+    users = newUsers;
+    localStorage.setItem("users", JSON.stringify(users));
+    localStorage.removeItem("currentUserEmail");
+    localStorage.removeItem("currentUser");
+}
+function deleteAccount(){
+    const currentEmail = localStorage.getItem("currentUserEmail");
+    if(!currentEmail) return;
+    deleteCurrentUserFromLocalStorage();
+    const deleteEmailInput = document.getElementById("deleteEmail");
+    if(deleteEmailInput){
+        deleteEmailInput.name="email";
+        deleteEmailInput.value = currentEmail;
+    }
+}
+if(deleteForm){
+    deleteForm.addEventListener("submit", deleteAccount);
+}
+
+                                                   // DEVICES //
+// const devicesForm = document.getElementById("devicesForm");
+// function displayAllDevices(){
+//     fetch("/devices")
+//     .then(response => response.json())      /* even though Flask sends JSON, the browser receives it as a raw HTTP response */
+//     .then(data =>{
+//         const deviceList = document.getElementById("devices-grid");
+//         if(!deviceList) return;
+//         deviceList.innerHTML="";   //clear the html to append mn awl w geded myb2ash feh garbage y3ny
+//         data.forEach(device =>{
+//             const deviceDiv = document.createElement("div");
+//             deviceDiv.className ="device-div";
+//             deviceDiv.innerHTML = device.image + "<br>" 
+//             + device.name + "<br>" 
+//             + device.description + "<br>" + 
+//             "EGP"+ device.price;
+//             deviceList.appendChild(deviceDiv);
+//         })
+
+//     })
+// }
+// if(devicesForm){
+//     devicesForm.addEventListener("submit", displayAllDevices)
+// }
+
+                                            // CART //
+const cartItems = document.getElementById("cart-items"); 
+const buttons = document.querySelectorAll(".add-to-cart-button");
+
+function goToCart(){
+    const user = localStorage.getItem("currentUserEmail");
+    if(!user){
+        window.location.href ="/login";
+    }
+    else{
+        window.location.href = "/cart";
+    }
+}
+
+
+function getCurrentUserEmail(){
+    return localStorage.getItem("currentUserEmail") || null;
+}
+
+function getCart(){
+    const email = getCurrentUserEmail();
+    if(!email) return [];
+    const cartData = sessionStorage.getItem("cart_" + email);
+    if(cartData)
+    { 
+        return JSON.parse(cartData);
+    }     
+    else{ 
+         return[]; 
+    }
+}
+
+function addToCart(product){
+    const cart = getCart();
+    cart.push(product);
+    saveCart(cart);
+}
+
+function removeFromCart(product){
+    let oldCart = getCart();
+    let newCart = [];
+    for(let i=0; i<oldCart.length; i++){
+        if(oldCart[i].name !== product){
+            newCart.push(oldCart[i]);
+        }
+    }
+    saveCart(newCart);
+    displayCart();
+}
+
+function clearCart(){
+    saveCart([]);
+    displayCart();
+}
+
+function displayCart(){
+    cartItems.innerHTML = ""; //clear the cart first
+    const cart = getCart();
+    if(!cartItems) return;
+    if(cart.length === 0){
+        cartItems.innerHTML = "<p>Your Cart is empty.</p>"
+        return;
+    }
+    else{
+        for(let i=0; i< cart.length; i++){
+            const item = cart[i];
+            
+            const card = document.createElement("div");
+            card.className="device-card";
+
+            const img = document.createElement("img");
+            img.src = "static/images/" + item.image;
+            img.alt = item.name;
+
+            const name = document.createElement("h3");
+            name.textContent = item.name;
+
+            const description = document.createElement("p");
+            description.textContent = item.description;
+
+            const price = document.createElement("p");
+            price.textContent = "EGP" + item.price;
+
+            const button = document.createElement("button");
+            button.textContent = "Remove";
+            button.onclick = function(){
+                removeFromCart(item.name);
+            };
+
+            card.appendChild(img);
+            card.appendChild(name);
+            card.appendChild(description);
+            card.appendChild(price);
+            card.appendChild(button);
+
+            cartItems.appendChild(card);
+        }
+    }
+}
+
+function saveCart(cart){
+    const email = getCurrentUserEmail();
+    if(!email) return;
+    sessionStorage.setItem("cart_"+ email , JSON.stringify(cart)); 
+}
+
+
+
+function addToCartButton(){
+    for(let i=0; i< buttons.length; i++){
+        buttons[i].addEventListener("click", function(){
+            const product = {
+                name: buttons[i].dataset.name,
+                description: buttons[i].dataset.description,
+                price: buttons[i].dataset.price,
+                image: buttons[i].dataset.image
+            };
+            addToCart(product);
+        });
+    }
+}
+document.addEventListener("DOMContentLoaded" , function(){
+    addToCartButton();
+    if(cartItems){
+        displayCart();
+    }
+})
