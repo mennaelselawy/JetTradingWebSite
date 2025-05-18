aa39f8935320955449569d104f658d721572d831
menna <mennnnaashraf855@gmail.com>
2025-05-18 17:52:04 +0300

    added productLoggedIn.html to make the logic make sense

--- app.py
+++ app.py
@@ -124,8 +124,6 @@ class User:
         updatedUser = self.makeDictionary()
         return self.fillProfileForm(updatedUser, "<p style='color:green;'>User updated successfully</p>")
 
-    
-    
     def deleteUser(self):
         try:
             with open("data/users.json", "r") as f:
@@ -210,7 +208,6 @@ def deleteUserAccount():
     return user.deleteUser()
 
 
-
                                                  #DEVICES
 class Devices:
     def __init__(self, name, description, price, image , category):
@@ -248,29 +245,32 @@ class Devices:
         return products
     
     @staticmethod
-    def display_devices_html(devices_list, show_cart_buttons=False):
+    def display_devices_html(devices_list, show_cart_buttons=False, logged_in = False):
         html=""
         for device in devices_list: 
-            # f """" = formatted string literals embed expressions directly into string literals using {}
             # i used the data- attribute so in javascript will get it by dataset.
             # the quote library for the spaces in the name of the device to put it in teh url without spaces ht7ot mkan l space %
-            html += f""" 
-                <div class="device-card "device-card-link"" >  
-                   <a href="/product?name={quote(device.name)}">
-                     <img src="static/images/{device.image}" alt="{device.name}" class="device-img">
-                    </a>
-                    <h3 class="device-name">{device.name}</h3>
-                    <p class="device-description">{device.description}</p>
-                    <p class="device-price">EGP {device.price}</p> """
+            if logged_in:
+                product_page_path = "/productLoggedIn"
+            else:
+                product_page_path = "/product"
+            html += f"""    
+               <div class="device-card">
+                    <a href="{product_page_path}?name={quote(device.name)}" class = "device-card-link">
+                        <img src="static/images/{device.image}" alt="{device.name}" class="device-img">
+                        </a>
+                        <h3 class="device-name">{device.name}</h3>
+                        <p class="device-description">{device.description}</p>
+                        <p class="device-price">EGP {device.price}</p> """
             if show_cart_buttons:    
-                html += f""" <button class="add-to-cart-button style="display:none;"
+                html += f""" <button class="add-to-cart-button"
                         data-name = "{device.name}"
                         data-description = "{device.description}"
                         data-price = "{device.price}"
                         data-image = "{device.image}">
                         Add to Cart
                     </button>"""
-            html += "</div> "
+            html += "</div>"
         return html
     
     @staticmethod
@@ -295,16 +295,15 @@ class Devices:
         else:
             filtered = devices
             heading = "All Devices"
-
-        devices_html = Devices.display_devices_html(filtered, showCartButtons)
-            
+        logged_in = (pageName == "devicesLoggedIn")
+        devices_html = Devices.display_devices_html(filtered, showCartButtons, logged_in)
+        
         if not devices_html:
             devices_html = "<p class='no-results'>No devices found.</p>"
             
         html = getHtml(pageName)
         final_html = html.replace("$$CATEGORY$$", heading).replace("$$DEVICES$$", devices_html)
         return final_html
-
 
         
 @app.route("/devices", methods=["GET"])
@@ -334,7 +333,37 @@ class Cart:
         self.items = []
     def get_items(self):
         return self.items
-    
+    @staticmethod
+    def productDetails(logged_in=False):
+        product_name = request.args.get("name")
+        devices = Devices.parse_products_file()
+        for device in devices:
+            if device.name.lower() == product_name.lower():
+                if logged_in:
+                    html = getHtml("productLoggedIn")
+                else:
+                    html = getHtml("product")
+                html = html.replace("$$NAME$$", device.name)
+                html = html.replace("$$DESCRIPTION$$", device.description)
+                html = html.replace("$$PRICE$$", device.price)
+                html = html.replace("$$IMAGE$$", f"static/images/{device.image}")
+                html = html.replace("$$CATEGORY$$", device.category.capitalize())
+                if logged_in:
+                        # Add proper data attributes for JavaScript
+                    add_to_cart_html = f"""
+                    <button class="add-to-cart-button"
+                        data-name="{device.name}"
+                        data-description="{device.description}"
+                        data-price="{device.price}"
+                        data-image="{device.image}">
+                        Add to Cart
+                    </button>"""
+                    html = html.replace("$$ADD_TO_CART$$", add_to_cart_html)
+                else:
+                    html = html.replace("$$ADD_TO_CART$$","")
+                return html
+        return "<p>Product not found</p>"
+
 @app.route("/products_json")
 def products_json():
     products_list = Devices.parse_products_file()
@@ -349,17 +378,9 @@ def cartPage():
     return html
 
 @app.route("/product", methods=["GET"])
-def productDetails():
-    product_name = request.args.get("name")
-    devices = Devices.parse_products_file()
-
-    for device in devices:
-        if device.name.lower() == product_name.lower():
-            html = getHtml("product")
-            html = html.replace("$$NAME$$", device.name)
-            html = html.replace("$$DESCRIPTION$$", device.description)
-            html = html.replace("$$PRICE$$", device.price)
-            html = html.replace("$$IMAGE$$", f"static/images/{device.image}")
-            html = html.replace("$$CATEGORY$$", device.category.capitalize())
-            return html
-    return "<p>Product not found</p>"
+def productPage():
+    return Cart.productDetails(logged_in=False)
+    
+@app.route("/productLoggedIn", methods=["GET"])
+def productLoggedInPage():
+    return Cart.productDetails(logged_in=True)
